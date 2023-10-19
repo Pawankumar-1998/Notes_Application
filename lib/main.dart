b7bf1115f09f7b05ba199e0ee1ec40288ebfad30
Pawan <paletipawankumar@gmail.com>
2023-10-20 01:47:18 +0530

    adding the home page code to the repo

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
-import 'package:mynotes/views/login_view.dart';
+// import 'package:mynotes/views/login_view.dart';
 
 void main() {
   // widget binding inplace
@@ -25,7 +25,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home"),
+        title: const Text("Home Page"),
       ),
       body: FutureBuilder(
         future: Firebase.initializeApp(
